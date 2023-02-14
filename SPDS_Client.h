bc9671fbece4e2bf404a788922b09d3e8259bbd1
Geckoops <862325068@qq.com>
2023-02-14 17:28:44 +0800

    可视化

--- SPDS_Client.h
+++ SPDS_Client.h
@@ -30,9 +30,26 @@ public slots:
     void on_maximize_clicked();
     void on_serverConfigButton_clicked();
 
+private slots:
+    void on_leftMenuTreeWidget_itemClicked(QTreeWidgetItem* indexItem, int itemID);
+
 private:
+    void showDetection();
+    void showVisualization();
+    void showFamilial();
+
+private:
+    enum Location
+    {
+        Wait_For_Init,
+        Loc_Detection,
+        Loc_Visualization,
+        Loc_Familial
+    };
+
+    Location location;
     Ui::SPDS_ClientClass ui;
     //´°Ìå¿ÉÒÆ¶¯
     QPoint pLast;
-    bool m_bPressed;
+    bool m_bPressed; 
 };
