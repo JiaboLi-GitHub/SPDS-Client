b1df1e34967193641041f8d8f890d3681c367d0d
JiaboLi-GitHub <1955474265@qq.com>
2023-03-11 20:33:04 +0800

    测试

--- HttpServer.cpp
+++ HttpServer.cpp
@@ -3,8 +3,9 @@
 #include "TcpSocket.h"
 #include "ServerConfig.h"
 #include "Detection.h"
+#include<qfile.h>
 
-const QString HttpServer::URL= "http://127.0.0.1:8989";
+const QString HttpServer::URL= "http://10.225.48.134:8989/image";
 
 HttpServer::HttpServer()
 {
@@ -29,10 +30,13 @@ void HttpServer::init()
 //检测请求
 void HttpServer::post(SPDOnceData data)
 {
-    QByteArray byteArray = "image=";
-	byteArray.append(JsonServer::toHTTPQByteArray(data));
-    qDebug() << byteArray;
-	QNetworkReply* reply = nam->post(*request, byteArray);
+ 	auto byteArray=JsonServer::toHTTPQByteArray(data);
+    QFile file("C:\\Users\\LJB\\Desktop\\text.txt");
+    file.open(QIODevice::ReadOnly);
+
+    QByteArray imageBytes = file.readAll();
+   // qDebug() << byteArray;
+	QNetworkReply* reply = nam->post(*request, imageBytes);
 }
 
 //检测完成后调用的槽函数
